8030ec7593b5c7e3f2a5f3abece7d01ee4956201
Dimitrios Kalemis <jimly1209@yahoo.gr>
2023-02-23 00:30:14 +0200

    connected rabbitmq to mosquitto

--- src/main/java/com/jss/config/CamelConfiguration.java
+++ src/main/java/com/jss/config/CamelConfiguration.java
@@ -1,8 +1,11 @@
 package com.jss.config;
 
 import com.rabbitmq.client.ConnectionFactory;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
 
 @Configuration
 public class CamelConfiguration {
@@ -18,5 +21,16 @@ public class CamelConfiguration {
         factory.setPassword("guest");
         return factory;
     }
-    //this is just a comment
+
+//    public MqttPahoClientFactory mqttPahoClientFactory() {
+//        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+//        MqttConnectOptions options = new MqttConnectOptions();
+//
+//        options.setServerURIs(new String[] {"tcp://localhost:1883"});
+//        options.setCleanSession(true);
+//
+//        factory.setConnectionOptions(options);
+//
+//        return factory;
+//    }
 }
